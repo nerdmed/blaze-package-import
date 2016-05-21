51ad1d6619446902e6cbce7efff81e9f0235052c
Ahmed Hassan <a.hassan@buismed.de>
2016-05-21 17:26:51 +0200

    inital commit

---
+++ packages/test/test.js
@@ -0,0 +1,6 @@
+import './test-temp.js';
+// Write your package code here!
+
+// Variables exported by this module can be imported by other packages and
+// applications. See test-tests.js for an example of importing.
+export const name = 'test';
